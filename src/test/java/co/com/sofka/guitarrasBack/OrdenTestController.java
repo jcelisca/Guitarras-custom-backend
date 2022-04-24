4b6caeda3935cd68f820c496182e5011c12432f5
jcelisca <celiss1997@gmail.com>
2022-04-24 09:16:16 -0500

    Test para casos de uso entidad Orden

---
+++ src/test/java/co/com/sofka/guitarrasBack/OrdenTestController.java
@@ -0,0 +1,66 @@
+package co.com.sofka.guitarrasBack;
+
+import co.com.sofka.guitarrasBack.application.service.OrdenService;
+import co.com.sofka.guitarrasBack.domain.Guitarra;
+import co.com.sofka.guitarrasBack.domain.Orden;
+import co.com.sofka.guitarrasBack.infraestructure.db.springdata.dto.OrdenDTO;
+import co.com.sofka.guitarrasBack.infraestructure.rest.spring.controller.OrdenController;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+
+@WebFluxTest
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes ={Orden.class, OrdenDTO.class, OrdenController.class})
+public class OrdenTestController {
+
+    @MockBean
+    private OrdenService service;
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Test
+    @DisplayName("POST / Guardar orden")
+    void guardarOrden(){
+        Orden orden = new Orden();
+        orden.setId("gyig5");
+        orden.setIdGuitarra("hiu4h");
+        orden.setComprobante("6235462354765");
+        orden.setUID("igy3i4kh34uobio");
+
+        var mono = Mono.just(modelMapper().map(orden, Orden.class));
+        when(service.save(any())).thenReturn(mono);
+
+        webTestClient.post()
+                .uri("/orden/crear")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(mono), Orden.class)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Orden.class)
+                .value(response->{
+                    Assertions.assertEquals("gyig5",response.getId());
+                    Assertions.assertEquals("hiu4h",response.getIdGuitarra() );
+                    Assertions.assertEquals("6235462354765", response.getComprobante());
+                    Assertions.assertEquals("igy3i4kh34uobio", response.getUID());
+                });
+    }
+}
---
+++ src/test/java/co/com/sofka/guitarrasBack/OrdenTestService.java
@@ -0,0 +1,75 @@
+package co.com.sofka.guitarrasBack;
+
+import co.com.sofka.guitarrasBack.application.repository.OrdenRepository;
+import co.com.sofka.guitarrasBack.application.service.OrdenService;
+import co.com.sofka.guitarrasBack.domain.Orden;
+import co.com.sofka.guitarrasBack.infraestructure.db.springdata.dto.OrdenDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@WebFluxTest
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes ={Orden.class, OrdenDTO.class, OrdenService.class})
+public class OrdenTestService {
+
+    @MockBean
+    private OrdenRepository repository;
+
+    @Autowired
+    private OrdenService service;
+
+    ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Test
+    @DisplayName("Guardar orden")
+    void guardar(){
+        Orden orden = new Orden();
+        orden.setId("gyig5");
+        orden.setIdGuitarra("hiu4h");
+        orden.setComprobante("6235462354765");
+        orden.setUID("igy3i4kh34uobio");
+
+        when(repository.save(any())).thenReturn(Mono.just(orden));
+
+        var mono = service.save(orden);
+
+        Assertions.assertEquals("gyig5", mono.block().getId());
+        Assertions.assertEquals("hiu4h", mono.block().getIdGuitarra());
+        Assertions.assertEquals("6235462354765", mono.block().getComprobante());
+        Assertions.assertEquals("igy3i4kh34uobio", mono.block().getUID());
+    }
+
+    @Test
+    @DisplayName("Mostrar todas laas ordenes")
+    void mostrarTodo(){
+        Orden orden = new Orden();
+        orden.setId("gyig5");
+        orden.setIdGuitarra("hiu4h");
+        orden.setComprobante("6235462354765");
+        orden.setUID("igy3i4kh34uobio");
+
+        when(repository.findAll()).thenReturn(Flux.just(orden));
+
+        var mono = service.findAll();
+
+        Assertions.assertEquals("gyig5", mono.blockFirst().getId());
+        Assertions.assertEquals("hiu4h", mono.blockFirst().getIdGuitarra());
+        Assertions.assertEquals("6235462354765", mono.blockFirst().getComprobante());
+        Assertions.assertEquals("igy3i4kh34uobio", mono.blockFirst().getUID());
+    }
+}
